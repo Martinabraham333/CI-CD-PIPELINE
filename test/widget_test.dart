9fe25968ac07e2a5eecf15f28d567fbbfd4572a2
exactusflutter01 <exactusflutterteam@gmail.com>
2025-12-12 17:15:28 +0530

    test4

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,26 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:counter_app/counter_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:counter_app/main.dart';
-
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('Initial count must be 0', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: CounterScreen())); // load widgete
+    expect(find.text("Count : 0"), findsOneWidget); // Check if count became 0
+  });
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  testWidgets('Increment button increases count', (tester) async {
+    await tester.pumpWidget(MaterialApp(home: CounterScreen()));
+    await tester.tap(find.byIcon(Icons.add)); // it tap add icon
+    await tester.pump(); // after taping and incrementing count to one , it refresh screen
+    expect(find.text("Count : 1"), findsOneWidget); // Check if count became 1
+  });
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+  testWidgets("Decrement button decreases count", (tester) async {
+    await tester.pumpWidget(const MaterialApp(home: CounterScreen()));
+    await tester.tap(find.byIcon(Icons.remove));
+    await tester.tap(find.byIcon(Icons.remove));
     await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text("Count : -1"), findsOneWidget);
   });
 }
